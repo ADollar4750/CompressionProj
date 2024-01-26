9a14e31556f799f485dcfe058d648210d1dda490
Alexander Dollar <alexander.dollar@students.snow.edu>
2024-01-26 16:46:55 -0700

    added nunit files

---
+++ UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace CompressionProj;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
