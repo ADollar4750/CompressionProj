30123a27ad06b57c22b344a747b2c55d0cbf964f
ADollar4750 <alextdollar@gmail.com>
2024-01-26 17:57:42 -0700

    test

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,15 +1,45 @@
 namespace CompressionProj;
 
-public class Tests
-{
+public interface IBitStringCompressor
+    {
+        string Compress(string original);
+        string Decompress(string compressed);
+    }   
+
+    public string Compress(string original)
+    {
+        string compressed;
+        char[] stringBroken = ;
+        
+        for(int i = 0; i < stringBroken.Length; i++)
+        {
+
+            if()
+            {
+
+            }
+        }
+        return compressed
+    }
+    public string Decompress(string compressed)
+    {
+
+    }
+
     [SetUp]
     public void Setup()
     {
     }
 
     [Test]
-    public void Test1()
+    public void CompressionTest()
     {
+        compressed assert.equal(00a111b)
         Assert.Pass();
     }
-}
+    [Test]
+    public void DecompressionTest()
+    {
+
+        Assert.Pass();
+    }
