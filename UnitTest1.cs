a2badc186d8e4bad8d4e4737d564d36da4c87dae
ADollar4750 <alextdollar@gmail.com>
2024-02-01 17:26:30 -0700

    Compression works

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,42 +1,56 @@
+using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Text;
 
 namespace CompressionProj;
 
 public interface IBitStringCompressor
-    {
-        string Compress(string original);
-        string Decompress(string compressed);
-    }   
-    public string 
+{
+    string Compress(string original);
+    string Decompress(string compressed);
+}
+public class EncodeDecode : IBitStringCompressor
+{
     public string Compress(string original)
     {
-        string compressed;
-        char[] stringBroken = ;
-        
-        for(int i = 0; i < stringBroken.Length; i++)
+        string compressed = "";
+        char[] stringBroken = original.ToCharArray();
+
+        for (int i = 0; i < original.Length; i++)
         {
-
-            if()
+            if (i+1<original.Length && stringBroken[i] != stringBroken[i+1])
             {
-
+                compressed+= stringBroken[i];
+                continue;
+            }
+            else if(i+1<original.Length && stringBroken[i] == stringBroken[i+1])
+            {
+                compressed+= stringBroken[i];
+                i++;
+            }
+            else
+            {
+                compressed+= stringBroken[i];
             }
         }
         return compressed;
     }
     public string Decompress(string compressed)
     {
-        text_Byte = compressed.Compress();//waiting for compress fuction
-        char[13] output;
+        // text_Byte = compressed.Compress();//waiting for compress fuction
+        // char[13] output;
 
-        foreach(char i in text_Byte)
-        {
-            output.Append(i);
-        }
-        return output;
-  
+        // foreach (char i in text_Byte)
+        // {
+        //     output.Append(i);
+        // }
+        // return output;
+        return "";
     }
-
+}
+public class Test
+{
     [SetUp]
     public void Setup()
     {
@@ -46,17 +60,20 @@ public interface IBitStringCompressor
     [Test]
     public void CompressionTest()
     {
-        string umcompressed;
-        string compressed = IBitStringCompressor.Compress(uncompressed);
-        Assert.AreEqual("13", compressed);
+        EncodeDecode test = new EncodeDecode();
+        string uncompressed = "01000011101011";
+        string result = test.Compress(uncompressed);
+        Assert.AreEqual("0100110101", result);
     }
-    //Decompression Test
-    [Test]
-    public void DecompressionTest()
-    {
-        string uncompressed;
-        string compressed = IBitStringCompressor.Compress(uncompressed);
+    // Decompression Test
+    //Syntax error
+    // [Test]
+    // public void DecompressionTest()
+    // {
+    //     string uncompressed;
+    //     string compressed = IBitStringCompressor.Compress(uncompressed);
 
-        string decompressed = IBitStringCompressor.Decompress(compressed);
-        Assert.AreEqual(uncompressed, decompressed);
-    }
+    //     string decompressed = IBitStringCompressor.Decompress(compressed);
+    //     Assert.AreEqual(uncompressed, decompressed);
+    // }
+}
